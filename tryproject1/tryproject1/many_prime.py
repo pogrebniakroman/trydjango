21aa2a49496293aee90082bc811ca793446028f7
pogrebniakroman <pogrebniakroman@gmail.com>
2023-04-08 20:23:20 +0200

    linear

---
+++ tryproject1/tryproject1/many_prime.py
@@ -0,0 +1,38 @@
+number = int(input('Enter prime check number: '))  # Enter value
+
+def isPrime_checker(number):
+    if number <= 1:
+        return False                               # Check rule for value
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+        else:
+            return True
+    return isPrime_checker(number)
+print('Prime check number: ', isPrime_checker(number))
+#This function is supposed to generate those many prime numbers, as taken as input argument 'num'
+
+num = int(input('Enter range number: '))
+
+def prime_checker(num):
+    prime_checker_list = []
+    i = 0
+    while len(prime_checker_list) <= num:
+        prime_checker_list.append(i)
+        i = i + 1
+
+    for j in range(1, num):
+        if num % prime_checker_list[j] == 0:
+            print(j)
+    return prime_checker_list
+print(prime_checker(num))
+
+
+
+
+
+
+
+
+
+
---
+++ tryproject1/tryproject1/prime_list1.py
@@ -0,0 +1,46 @@
+"""
+number = int(input('Enter prime check number: '))  # Enter value
+
+def isPrime_checker(number):
+    if number <= 1:
+        return False                               # Check rule for value
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+        else:
+            return True
+    return isPrime_checker(number)
+print('Prime check number: ', isPrime_checker(number))
+#This function is supposed to generate those many prime numbers, as taken as input argument 'num'
+
+num = int(input('Enter range number: '))
+
+def prime_checker(num):
+    prime_checker_list = []
+    i = 0
+    while len(prime_checker_list) < num - 2:
+         if i > 1:
+            prime_checker_list.append(i)
+         i = i + 1
+    return prime_checker_list
+print(prime_checker(num))
+
+def checker():
+    prime_list = []
+    i = 1
+    for i in range(num):
+        if isPrime_checker(i) is True:
+            prime_list.append(i)
+        i = i + 1
+    return prime_list
+
+print(checker())
+"""
+
+print('First programm- Python programm function')
+print('It is declared like this: ')
+print("print('what to print')")
+print("Hello\nHello python\nPikus\n")
+print(6*"Roman\'s Lectures\n")
+
+
