bd1d4a92dd9421dfcdfd9a86c47f6e665baac397
pogrebniakroman <pogrebniakroman@gmail.com>
2023-04-13 08:50:25 +0200

    linear

--- tryproject1/tryproject1/string_function1 (2).py
+++ tryproject1/tryproject1/string_function1 (2).py
@@ -8,6 +8,6 @@ string_list(string)
 def string_list_back(string):           #This function the reversed string
     return string[::-1]                 #A loop isn't required when 
 string_list_back(string)                #using this method
-
+print(string_list_back(string))
 
 
